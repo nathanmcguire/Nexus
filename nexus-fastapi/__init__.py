fc98753ca70e33f7fcec702f65a9818b129783f9
Nathan C. McGuire <nathan@ncm.me>
2025-04-10 16:03:51 -0500

    Update fastapi

--- nexus-fastapi/__init__.py
+++ nexus-fastapi/__init__.py
@@ -1,14 +1,1 @@
-from fastapi import FastAPI
-
-# Create an instance of FastAPI
-app = FastAPI()
-
-# Define a basic route
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-
-# Define another route that accepts a path parameter
-@app.get("/greet/{name}")
-def greet_user(name: str):
-    return {"message": f"Hello, {name}!"}
+from .main import app
