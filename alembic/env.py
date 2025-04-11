df35654a467599f011e035d1686bdad9799cbeb5
Nathan C. McGuire <nathan@ncm.me>
2025-04-11 14:52:16 -0500

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from nexus-fastapi.database import Base
+from nexus_fastapi.database import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
