72ac32231b340be0047d3847daef8703b256e8fd
Nathan C. McGuire <nathan@ncm.me>
2025-04-15 21:01:45 -0500

    Init Swift Multiplatform project

---
+++ nexus_darwin/nexusTests/nexusTests.swift
@@ -0,0 +1,16 @@
+//
+//  nexusTests.swift
+//  nexusTests
+//
+//  Created by Nathan McGuire on 4/15/25.
+//
+
+import Testing
+
+struct nexusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
