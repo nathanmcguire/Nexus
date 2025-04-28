7f7ae3abb806b2dc62eb893390c4ae9b19caebdd
Nathan C. McGuire <nathan@ncm.me>
2025-04-28 16:17:24 -0500

    Update main.py

--- nexus_fastapi/main.py
+++ nexus_fastapi/main.py
@@ -21,22 +21,22 @@ app = FastAPI(
         "name": "",
         "identifier": "MIT",
     },
-    openapi_tags=[
-        {"name": "Users", "description": "User Management", "endpoints": [
-            {"operationId": "getUsers", "summary": "Get Users"},
-            {"operationId": "createUser", "summary": "Create User"},
-            {"operationId": "getUserById", "summary": "Get User By Id"},
-            {"operationId": "replaceUserById", "summary": "Replace User By Id"},
-            {"operationId": "updateUserById", "summary": "Update User By Id"},
-            {"operationId": "deleteUserById", "summary": "Delete User By Id"},
-        ]},
-        {"name": "Authentication", "description": "Authentication", "endpoints": [
-            {"operationId": "register", "summary": "Register"},
-            {"operationId": "login", "summary": "Login"},
-            {"operationId": "logout", "summary": "Logout"},
-            {"operationId": "token", "summary": "Token"},
-        ]}
-    ]
+    # openapi_tags=[
+    #     {"name": "Users", "description": "User Management", "endpoints": [
+    #         {"operationId": "getUsers", "summary": "Get Users"},
+    #         {"operationId": "createUser", "summary": "Create User"},
+    #         {"operationId": "getUserById", "summary": "Get User By Id"},
+    #         {"operationId": "replaceUserById", "summary": "Replace User By Id"},
+    #         {"operationId": "updateUserById", "summary": "Update User By Id"},
+    #         {"operationId": "deleteUserById", "summary": "Delete User By Id"},
+    #     ]},
+    #     {"name": "Authorizations", "description": "Authorizations", "endpoints": [
+    #         {"operationId": "register", "summary": "Register"},
+    #         {"operationId": "login", "summary": "Login"},
+    #         {"operationId": "logout", "summary": "Logout"},
+    #         {"operationId": "token", "summary": "Token"},
+    #     ]}
+    # ]
 )
 
 app.add_middleware(
